740431b9d14d83c242d2a3e129979a09a529ca0e
Akmaljon Fayzullayev <akmaljonfayzullayev07@gmail.com>
2023-05-09 20:03:39 +0500

    yangiladim

--- asosiy/models.py
+++ asosiy/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Tarif(models.Model):
+    nom = models.CharField(max_length=30)
+    muddat = models.CharField(max_length=30)
+    narx = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return self.nom

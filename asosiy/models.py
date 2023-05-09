897e7cd174009008b840c9b9ec9d011584e8813c
Akmaljon Fayzullayev <akmaljonfayzullayev07@gmail.com>
2023-05-09 20:07:41 +0500

    men ham yangiladim

--- asosiy/models.py
+++ asosiy/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Aktyor(models.Model):
+    ism = models.CharField(max_length=30)
+    davlat = models.CharField(max_length=30, blank=True)
+    tugilgan_yil = models.DateField()
+    jins = models.CharField(max_length=7)
+    def __str__(self):
+        return self.ism
+
+class Kino(models.Model):
+    nom = models.CharField(max_length=50)
+    janr = models.CharField(max_length=50)
+    yil = models.DateField()
+    aktyorlar = models.ManyToManyField(Aktyor)
+    def __str__(self):
+        return self.nom
